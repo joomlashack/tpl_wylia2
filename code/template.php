db63f5054ae178a5b632d66126819055625219ec
jmarkos <jkmarcosb@gmail.com>
2013-05-06 11:20:46 -0600

    fixing width footer

--- code/template.php
+++ code/template.php
@@ -168,12 +168,12 @@ if ($this->countModules('sidebar1') && $this->countModules('sidebar2')){
                     }
                 }
                 navPosition();
-                function footerWidth(){
+                 function footerWidth(){
                     var wrapperFooter = jQuery('.wrapper-footer');
                     var Footer = jQuery('#footer');
                     var wrapperFooterWidth = wrapperFooter.width();
                     console.log(wrapperFooterWidth);
-                    Footer.css('width',wrapperFooterWidht);
+                    Footer.css('width',wrapperFooterWidth);
                 }
                 footerWidth();
                 jQuery(window).resize(function() {
