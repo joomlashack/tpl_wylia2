d5d8892fbd1e4ac739788f0e2805ca5641387398
Tito Alvarez <augustoalvarez@gmail.com>
2013-08-07 17:38:32 -0600

    Compatibility with Wright 3.1 Fixed credits Added home.php switch if custom.php is present

--- code/functions.php
+++ code/functions.php
@@ -1,11 +1,41 @@
 <?php
 /**
- * @copyright	Copyright (C) 2005 - 2011 Joomlashack / Meritage Assets
- * @author		Jeremy Wilken - Joomlashack
- * @package		Wright
+ * @package     Wylia
+ * @subpackage  Functions
  *
- * Use this file to add any PHP to the template before it is executed
+ * @copyright   Copyright (C) 2005 - 2013 Joomlashack. Meritage Assets.  All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
- 
+
 // Restrict Access to within Joomla
 defined('_JEXEC') or die('Restricted access');
+
+// get the bootstrap row mode ( row / row-fluid )
+$gridMode = $this->params->get('bs_rowmode','row-fluid');
+$containerClass = 'container';
+if ($gridMode == 'row-fluid') {
+    $containerClass = 'container-fluid';
+}else{
+    $fixedClass = ' fixed';
+    $contentFixed = ' content-fixed';
+    $footerWrapperClass = ' container';
+}
+
+$bodyclass = "";
+if ($this->countModules('toolbar')) {
+    $bodyclass = "toolbarpadding";
+}
+$responsivePage = $this->params->get('responsive','1');
+$responsive = ' responsive';
+if ($responsivePage == 0) {
+    $responsive = ' no-responsive';
+}
+if ($this->countModules('sidebar1') && $this->countModules('sidebar2')){
+    $sidebarClass= ' sb1 sb2';
+}elseif ($this->countModules('sidebar2')) {
+        $sidebarClass = ' sb2';
+}elseif ($this->countModules('sidebar1')) {
+        $sidebarClass = ' sb1';
+}
+
+$moduleMain = ($this->params->get('wylia_module_main','0') == '1' ? true : false);
