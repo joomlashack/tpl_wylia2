1edb0c3d700fd983f15cc92a8c011d17c0245d68
jmarkos <markos@joomlashack.com>
2015-01-02 17:28:40 +0100

    adding equalheighs to ie8/9

--- code/js/fallback.js
+++ code/js/fallback.js
@@ -1,29 +1,15 @@
 jQuery(document).ready(function($) {
 
-	function setImages(img,parent,attr){
-		jQuery(img).each(function (i){
-			jQuery(this).hide();
-			var newWidth = jQuery(parent).width() - 20;
-			jQuery(this).show();
-			jQuery(this).css(attr , newWidth + 'px');
-		});
-	}
+    function setBlogItems(){
+        jQuery("#main-content > *").equalHeights();
+    }
 
-	jQuery(window).load(function(){
-		if (jQuery(".is_internet.v_8").length) {
-			setImages('#sidebar1 img', '#sidebar1', 'width');
-			setImages('#sidebar2 img', '#sidebar2', 'width');
-		}
-		if (jQuery(".is_internet.v_9").length) {
-			setImages('#sidebar1 img', '#sidebar1', 'max-width');
-			setImages('#sidebar2 img', '#sidebar2', 'max-width');
-		}
-	});
-	jQuery(window).resize(function(){
-		if (jQuery(".is_internet.v_9").length) {
-			setImages('#sidebar1 img', '#sidebar1', 'max-width');
-			setImages('#sidebar2 img', '#sidebar2', 'max-width');
-		}
-	});
+    jQuery(window).load(function(){
+        setBlogItems();
+    });
+    jQuery(window).resize(function(){
+        if (jQuery(".is_internet.v9").length)
+            setBlogItems();
+    });
 
 });
--- code/template.php
+++ code/template.php
@@ -158,6 +158,7 @@ defined('_JEXEC') or die('Restricted access');
                 $major = $browser->getMajor();
 
                 if ((int)$major <= 9) {
+                    echo "<script type='text/javascript' src='" . JURI::root() .  "templates/" . $this->document->template . "/js/jquery.equalheights.js'></script>";
                     echo "<script type='text/javascript' src='" . JURI::root() .  "templates/" . $this->document->template . "/js/fallback.js'></script>";
                 }
 
